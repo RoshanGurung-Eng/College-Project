0b49d7c93eb64e03b393faf33145e0c101c9cd29
RoshanGurung-Eng <roshantheaomine@gmail.com>
2024-10-15 20:35:38 +0545

    app.jsx changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-// src/App.jsx
+import React from "react";
+
 function App() {
-  return () =>{
-    pass
-  }
+  return (
+    <div className="bg-neutral-700">
+      <h1 className="text-5xl text-red-700">This is your first page</h1>
+      <p>Let's start, boy, it's time to do it!</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
